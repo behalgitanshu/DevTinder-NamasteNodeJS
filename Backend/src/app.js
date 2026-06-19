cb42082c5365c702a53242102a45b7b60f938c76
Gitanshu Behal <behalgitanshu@gmail.com>
2026-06-19 12:45:48 +0530

    Changes in Backend to whitelist frontend in CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		credentials: true,
+	}),
+);
 
 const healthRoutes = require("./routes/health");
 const authRoutes = require("./routes/auth");
