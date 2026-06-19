e5b41a5f0595744b69c21c5d2f8ffa9e24b70ab9
Gitanshu Behal <behalgitanshu@gmail.com>
2026-06-19 13:20:49 +0530

    Auth Handling on Frontend Side

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import Body from "./layout/Body";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
-import appStore from "./utils/appStore";
-import { Provider } from "react-redux";
 import Feed from "./pages/Feed";
+import appStore from "./utils/appStore";
+
+const RequireAuth = ({ children }) => {
+	const user = useSelector((state) => state.user);
+	return user ? children : <Navigate to="/login" replace />;
+};
+
+const RedirectIfAuth = ({ children }) => {
+	const user = useSelector((state) => state.user);
+	return user ? <Navigate to="/feed" replace /> : children;
+};
 
 function App() {
 	return (
@@ -12,9 +23,38 @@ function App() {
 			<BrowserRouter basename="/">
 				<Routes>
 					<Route path="/" element={<Body />}>
-						<Route path="/login" element={<Login />} />
-						<Route path="/feed" element={<Feed />} />
-						<Route path="/profile" element={<Profile />} />
+						<Route
+							index
+							element={
+								<RequireAuth>
+									<Navigate to="/feed" replace />
+								</RequireAuth>
+							}
+						/>
+						<Route
+							path="/login"
+							element={
+								<RedirectIfAuth>
+									<Login />
+								</RedirectIfAuth>
+							}
+						/>
+						<Route
+							path="/feed"
+							element={
+								<RequireAuth>
+									<Feed />
+								</RequireAuth>
+							}
+						/>
+						<Route
+							path="/profile"
+							element={
+								<RequireAuth>
+									<Profile />
+								</RequireAuth>
+							}
+						/>
 						<Route path="*" element={<div>Page Not Found</div>} />
 					</Route>
 				</Routes>
--- Frontend/src/layout/Body.jsx
+++ Frontend/src/layout/Body.jsx
@@ -1,8 +1,44 @@
+import React from "react";
 import NavBar from "./NavBar";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Body = () => {
+	const dispatch = useDispatch();
+	const user = useSelector((state) => state.user);
+	const [loading, setLoading] = React.useState(!user);
+
+	const fetchUser = React.useCallback(async () => {
+		try {
+			const result = await axios.get(BASE_URL + "/profile/view", {
+				withCredentials: true,
+			});
+			dispatch(addUser(result.data.user));
+		} catch {
+			dispatch(removeUser());
+		} finally {
+			setLoading(false);
+		}
+	}, [dispatch]);
+
+	React.useEffect(() => {
+		if (!user) {
+			fetchUser();
+		}
+	}, [user, fetchUser]);
+
+	if (loading) {
+		return (
+			<div className="flex items-center justify-center min-h-screen">
+				<span className="loading loading-spinner loading-lg text-primary" />
+			</div>
+		);
+	}
+
 	return (
 		<div className="flex flex-col min-h-screen">
 			<NavBar />
